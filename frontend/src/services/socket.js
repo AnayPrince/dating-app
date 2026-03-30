0e7624520fc29eb17486a6257f43de7f6d038acb
Anay Kumar <anay.kumar@sparinfosys.com>
2026-03-30 23:10:49 +0530

    fix cors and socket issue

--- frontend/src/services/socket.js
+++ frontend/src/services/socket.js
@@ -1,6 +1,15 @@
+// import { io } from "socket.io-client";
+
+// // const socket = io("http://localhost:5000");
+// const socket = io("https://dating-backend-d40u.onrender.com");
+
+// export default socket;
+
 import { io } from "socket.io-client";
 
-// const socket = io("http://localhost:5000");
-const socket = io("https://dating-backend-d40u.onrender.com");
+const socket = io("https://dating-backend-d40u.onrender.com", {
+  transports: ["websocket"], // 🔥 IMPORTANT FIX
+  withCredentials: true,
+});
 
 export default socket;
