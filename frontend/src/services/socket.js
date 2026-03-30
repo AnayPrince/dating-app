6b292b37a513e590c0442c02be85aa4dc8b16db3
Anay Kumar <anay.kumar@sparinfosys.com>
2026-03-30 13:15:55 +0530

    frontend deploy config

--- frontend/src/services/socket.js
+++ frontend/src/services/socket.js
@@ -1,5 +1,6 @@
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:5000"); // ✅ correct
+// const socket = io("http://localhost:5000");
+const socket = io("https://dating-backend-d40u.onrender.com");
 
 export default socket;
